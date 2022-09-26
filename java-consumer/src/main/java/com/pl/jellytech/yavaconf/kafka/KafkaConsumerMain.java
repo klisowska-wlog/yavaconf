70929458c209a152837108ed169478634c95d96c
klisowska-wlog <86469730+klisowska-wlog@users.noreply.github.com>
2022-09-26 23:00:37 +0200

    Added Java consumer client

---
+++ java-consumer/src/main/java/com/pl/jellytech/yavaconf/kafka/KafkaConsumerMain.java
@@ -0,0 +1,33 @@
+package com.pl.jellytech.yavaconf.kafka;
+
+import com.pl.jellytech.yavaconf.kafka.consumer.KafkaConsumerManager;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class KafkaConsumerMain {
+	private static final Logger logger = LogManager.getLogger(KafkaConsumerMain.class);
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main( String[] args ) {
+		logger.info("Started Kafka JAVA Consumer API example");
+
+		logger.info("Starting Kafka consumer. Please provide topics to subscribe to separated by ',':");
+		String topicsStr = scanner.nextLine();
+
+		logger.info("Please provide group id to assign consumer to:");
+		String groupId = scanner.nextLine();
+
+		KafkaConsumer<String, String> consumer = KafkaConsumerManager.createConsumer(groupId);
+		List<String> topics = Arrays.stream(topicsStr.split(",")).map(String::trim).collect(Collectors.toList());
+
+		KafkaConsumerManager.infinitelyConsume(consumer, topics);
+
+		logger.info("Finished reading messages from topics {}. Exiting application ...", topicsStr);
+	}
+}
---
+++ java-consumer/src/main/java/com/pl/jellytech/yavaconf/kafka/consumer/KafkaConsumerManager.java
@@ -0,0 +1,54 @@
+package com.pl.jellytech.yavaconf.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class KafkaConsumerManager {
+	private final static Logger logger = LogManager.getLogger(KafkaConsumerManager.class);
+
+	private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+
+	private static Map<String, Object> getProperties(){
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+		return props;
+	}
+
+	public static KafkaConsumer<String, String> createConsumer(String groupId){
+		Map<String, Object> props = getProperties();
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+//		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+		logger.debug("Starting new consumer for group {}", groupId);
+		return new KafkaConsumer<>(props);
+	}
+
+	public static void infinitelyConsume(KafkaConsumer<String, String> consumer, List<String> topics){
+		consumer.subscribe(topics);
+
+		try (consumer) {
+			while (true) {
+				ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+				records.forEach(record -> {
+					logger.debug("Record retrieved: Key: {}, value: {}, partition: {}, offset: {})",
+							record.key(), record.value(),
+							record.partition(), record.offset());
+					logger.info("Record retrieved: Key: {}, value: {}", record.key(), record.value());
+				});
+				consumer.commitSync();
+			}
+		}
+	}
+}
